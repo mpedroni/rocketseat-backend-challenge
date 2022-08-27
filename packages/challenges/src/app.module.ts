1af8bd34cb0f645f1a90c7105f7ceead11ead0a0
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-26 22:51:13 -0300

    feat: Add GraphQL support

--- packages/challenges/src/app.module.ts
+++ packages/challenges/src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ChallengeModule } from './modules/challenges/challenge.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ChallengeModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
