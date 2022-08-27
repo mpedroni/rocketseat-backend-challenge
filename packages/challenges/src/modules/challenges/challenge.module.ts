449fcac9ce6c0c77dd198b5e9e996873adf4b77d
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-26 22:56:45 -0300

    feat: Create the NestJS's Challenges module

---
+++ packages/challenges/src/modules/challenges/challenge.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { CreateChallengeUseCase } from 'src/@domain/usecases/create-challenge.usecase';
+import { ChallengeRepository } from 'src/@domain/usecases/ports/challenge.repository';
+import { UuidAdapter } from 'src/@domain/usecases/ports/uuid.adapter';
+import { PrismaChallengeRepository } from 'src/main/usecases/ports/prisma-challenge.repository';
+import { PrismaService } from 'src/prisma.service';
+import { UuidAdapterV4 } from 'src/uuid.service';
+import { ChallengeResolver } from './challenge.resolver';
+import { ChallengeService } from './challenge.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [
+    ChallengeResolver,
+    ChallengeService,
+    PrismaService,
+    {
+      provide: 'ChallengeRepository',
+      useClass: PrismaChallengeRepository,
+    },
+    {
+      provide: 'UuidAdapter',
+      useClass: UuidAdapterV4,
+    },
+    {
+      provide: CreateChallengeUseCase,
+      useFactory: (
+        challengeRepository: ChallengeRepository,
+        uuidAdapter: UuidAdapter,
+      ) => {
+        return new CreateChallengeUseCase(challengeRepository, uuidAdapter);
+      },
+      inject: ['ChallengeRepository', 'UuidAdapter'],
+    },
+  ],
+})
+export class ChallengeModule {}
