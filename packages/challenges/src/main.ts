da82ef5fea8203b7b85d8be6924b0642502b4f5f
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-29 11:54:23 -0300

    feat: Create a KafkaMicroservice on the app

--- packages/challenges/src/main.ts
+++ packages/challenges/src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
