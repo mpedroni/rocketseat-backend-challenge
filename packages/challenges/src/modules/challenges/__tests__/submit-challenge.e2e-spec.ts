25dc61a155ac8bcd17373810d3e87dad6446a313
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-30 14:07:46 -0300

    tests(e2e): Create an test to the SubmitChallenge Resolver

---
+++ packages/challenges/src/modules/challenges/__tests__/submit-challenge.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { createTestModule } from './utils/setup';
+
+describe('SubmitChallengeResolver (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await createTestModule();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to submit a existent Challenge', async () => {
+    const addChallengeQuery = `
+      mutation AddChallenge($input: CreateChallengeInput!) {
+        addChallenge(createChallengeInput: $input) {
+          id
+      }
+    }
+    `;
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        operationName: 'AddChallenge',
+        variables: {
+          input: {
+            title: 'Challenge Test',
+            description: 'This is a challenge description',
+          },
+        },
+        query: addChallengeQuery,
+      });
+
+    const submitChallengeQuery = `
+      mutation SubmitChallenge($input: SubmitChallengeInput!) {
+        submitChallenge(submitChallengeInput: $input) {
+          id
+          challenge_id
+          repository_url
+          status
+          grade
+        }
+      }
+    `;
+    const challengeId = response.body.data.addChallenge.id;
+    const input = {
+      challengeId,
+      repositoryUrl: 'https://github.com/mpedroni/rocketseat-backend-challenge',
+    };
+
+    const { body } = await request(app.getHttpServer()).post('/graphql').send({
+      operationName: 'SubmitChallenge',
+      variables: {
+        input,
+      },
+      query: submitChallengeQuery,
+    });
+
+    const data = body.data.submitChallenge;
+    expect(data.id).toBeTruthy();
+    expect(data.challenge_id).toEqual(input.challengeId);
+    expect(data.repository_url).toEqual(input.repositoryUrl);
+    expect(data.status).toEqual('Pending');
+    expect(data.grade).toEqual(null);
+  });
+});
