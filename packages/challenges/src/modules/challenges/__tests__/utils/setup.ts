fda38eef061954ce458b001d298323dc42da1109
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-30 14:03:14 -0300

    tests(e2e): Create a App factory helper
    Creates a NestApplication with the App and Challenge modules with mocks to repositories and KafkaService

---
+++ packages/challenges/src/modules/challenges/__tests__/utils/setup.ts
@@ -0,0 +1,31 @@
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { InMemoryChallengeRepository } from 'src/main/usecases/ports/in-memory-challenge.repository';
+import { InMemorySubmissionRepository } from 'src/main/usecases/ports/in-memory-submission.repository';
+import { ChallengeModule } from '../../challenges.module';
+
+const mockedKafkaService = {
+  connect: jest.fn(),
+  emit: jest.fn(),
+  close: jest.fn(),
+  subscribeToResponseOf: jest.fn(),
+  send: jest.fn(() => ({
+    subscribe: jest.fn(),
+  })),
+};
+
+export async function createTestModule() {
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule, ChallengeModule],
+  })
+    .overrideProvider('ChallengeRepository')
+    .useClass(InMemoryChallengeRepository)
+    .overrideProvider('SubmissionRepository')
+    .useClass(InMemorySubmissionRepository)
+    .overrideProvider('KafkaService')
+    .useValue(mockedKafkaService)
+    .compile();
+
+  const app = moduleRef.createNestApplication();
+  return app;
+}
